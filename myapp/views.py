b6bacfb2651cae582a7a83ec5520489ab98f5a83
krina1902 <krinaantala33@gmail.com>
2023-07-19 11:53:37 +0530

    dj

---
+++ myapp/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from .models import Contact
+
+# Create your views here.
+def index(request):
+	return render(request,'index.html')
+
+def contact(request):
+	if request.method=='POST':
+		Contact.objects.create(
+			name=request.POST['name'],
+			email=request.POST['email'],
+			mobile=request.POST['mobile'],
+			remarks=request.POST['remarks'],
+			)
+		msg="Contact Saved Successfully"
+		return render(request,'contact.html',{'msg':msg})
+	else:
+		return render(request,'contact.html')
+
+
+def signup(request):
+	return render(request, 'signup.html')
+
+def login(request):
+	return render(request,'login.html')
